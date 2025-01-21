a3e67bde114f627e70202e2e4a8c8eba08eab568
MFedatto <mfedatto@gmail.com>
2025-01-20 21:36:36 -0300

    Update ansible-playbook-service.sh

--- ansible-on-boot/ansible-playbook-service.sh
+++ ansible-on-boot/ansible-playbook-service.sh
@@ -20,6 +20,7 @@ PLAYBOOK_PATH="/home/mkadmin/ansible/boot-setup.yml"
 VAULT_FILE="/home/mkadmin/ansible/vars/mkadmin.yml"
 VAULT_PASSWORD_FILE="/home/mkadmin/ansible/vault_password.txt"
 SERVICE_FILE="/etc/systemd/system/ansible-playbook.service"
+BACKUP_SERVICE_FILE="/etc/systemd/system/ansible-playbook.service.bak.$TIMESTAMP"
 
 log() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" | tee -a $LOG_FILE
@@ -55,7 +56,16 @@ sudo chown mkadmin:mkadmin $VAULT_FILE
 log "Encrypting the Ansible Vault file..."
 sudo -u mkadmin ansible-vault encrypt --vault-password-file $VAULT_PASSWORD_FILE $VAULT_FILE &>> $LOG_FILE
 
-log "Creating or overwriting systemd service file..."
+if [ -f "$SERVICE_FILE" ]; then
+    log "Service file exists. Backing up and removing existing service..."
+    sudo cp $SERVICE_FILE $BACKUP_SERVICE_FILE &>> $LOG_FILE
+    sudo systemctl stop ansible-playbook.service &>> $LOG_FILE
+    sudo systemctl disable ansible-playbook.service &>> $LOG_FILE
+    sudo rm $SERVICE_FILE &>> $LOG_FILE
+    sudo systemctl daemon-reload &>> $LOG_FILE
+fi
+
+log "Creating new systemd service file..."
 sudo bash -c 'cat <<EOF > /etc/systemd/system/ansible-playbook.service
 [Unit]
 Description=Run Ansible Playbook at boot
@@ -77,7 +87,16 @@ log "Enabling Ansible service to run at boot..."
 sudo systemctl enable ansible-playbook.service &>> $LOG_FILE
 
 log "Starting Ansible service manually..."
-sudo systemctl start ansible-playbook.service &>> $LOG_FILE
+if sudo systemctl start ansible-playbook.service &>> $LOG_FILE; then
+    log "Service started successfully. Removing backup service file..."
+    sudo rm $BACKUP_SERVICE_FILE &>> $LOG_FILE
+else
+    log "Service failed to start. Restoring backup service file..."
+    sudo mv $BACKUP_SERVICE_FILE $SERVICE_FILE &>> $LOG_FILE
+    sudo systemctl daemon-reload &>> $LOG_FILE
+    sudo systemctl enable ansible-playbook.service &>> $LOG_FILE
+    sudo systemctl start ansible-playbook.service &>> $LOG_FILE
+fi
 
 log "Checking the status of the Ansible service..."
 sudo systemctl status ansible-playbook.service &>> $LOG_FILE
